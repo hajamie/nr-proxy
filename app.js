340a3ed2d6d80f94917c7610001827d82a1759f7
hajamie <hajamie@gmail.com>
2019-04-20 17:08:19 +0200

    Use PORT environment variable for listen port

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({port: 8080});
+const wsPort = process.env.PORT || 80;
+const wss = new WebSocket.Server({port: wsPort});
+console.log('Socket listening on ' + wsPort);
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
