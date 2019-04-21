36e3fdf141b1e60795f71963f77b5fb7858ac766
hajamie <hajamie@gmail.com>
2019-04-21 20:04:03 +0200

    Redirect HTTP to HTTPS. Better logging

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # nr-proxy
+Proxy HTTP requests over to a websocket. 
+
+  * Runs on Google App Engine (GAE)
+  * Has to be in the flexible environment so that websockets can connect
+
+## Prerequisites
+  * Download the [Google Cloud SDK](https://cloud.google.com/sdk/docs/)
+  * Create a project: `gcloud projects create [YOUR_PROJECT_NAME] --set-as-default`
+  * Initialise app: `gcloud app create --project=[YOUR_PROJECT_NAME]`
+  * [Enable billing](https://console.cloud.google.com/projectselector/billing?lang=nodejs&st=true)
+  * Install [Git](https://git-scm.com/)
+  * Install [Node.js](https://nodejs.org/)
+
+## Run the proxy
+```
+git clone https://github.com/hajamie/nr-proxy.git
+cd nr-proxy
+npm install
+npm run deploy
+```
+
+Take a look at http://YOUR_PROJECT_ID.appspot.com
+Create a node:
+```
+[{"id":"b3e9242.c5b60d8","type":"websocket in","z":"ba3995de.77a128","name":"nr-proxy","server":"","client":"1da91ba0.567e24","x":80,"y":40,"wires":[[]]},{"id":"1da91ba0.567e24","type":"websocket-client","z":"","path":"http://YOUR_PROJECT_ID.appspot.com","tls":"","wholemsg":"false"}]
+```
+
+## SSL setup
+HTTP is redirected to HTTPS when deployed to GAE.  
+[See Google's setup instructions](https://cloud.google.com/appengine/docs/flexible/nodejs/securing-custom-domains-with-ssl)
+
+## Useful tools
+  * [WebSocket Test Client](https://chrome.google.com/webstore/detail/websocket-test-client/fgponpodhbmadfljofbimhhlengambbn) Chrome extension
+  * [Postman](https://www.getpostman.com/downloads/) API Development Environment
--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const GOOGLE_CLOUD_PROJECT = process.env.GOOGLE_CLOUD_PROJECT || 'unknown';
 console.log('Starting ' + GOOGLE_CLOUD_PROJECT + '. Env: ' + NODE_ENV + '. Port: ' + PORT);
 
 // Enable App Engine debugging. 
-if (NODE_ENV == 'production') {
+if (NODE_ENV === 'production') {
   require('@google-cloud/debug-agent').start();
 }
 const WebSocket = require('ws');
@@ -15,31 +15,49 @@ const http = require('http');
 
 // Handle HTTP. 
 var server = http.createServer(function (request, response) {
-  console.log('Received request for ' + request.url);
+  // Startup request from GAE for manual scaling.  Ignored.  
+  if (request.url == '/_ah/start') {
+    response.writeHead(200);
+    response.end();
+    return;
+  }
+  
+  var schema = (request.headers['x-forwarded-proto'] || '').toLowerCase();
+  console.log('HTTP received request for ' + request.url + '. Schema: ' + schema);
+  
+  // Redirect to https on production. 
+  if (NODE_ENV === 'production' && schema !== 'https') {
+    response.writeHead(302, {'Location': 'https://' + request.headers.host + request.url});
+    response.end();
+    return;
+  }
+  
+  // Handle event requests. 
   if (request.url == '/event' && request.method === 'POST') {
     let body = '';
     request.on('data', chunk => {body += chunk.toString();});
     request.on('end', () => {
       try {
         var data = JSON.parse(body);
-        if (!data.event) {
-          response.writeHead(400, {'Content-Type': 'text/plain'});
-          response.end("Not an event");
-        }
-        console.log('Event: ' + data.event);
-        console.log('Clients: ' + wss.clients.size);
-        // Send the event to clients. 
-        wss.clients.forEach(function each(client) {
-          client.send(JSON.stringify({"event": data.event}));
-        });
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.end(JSON.stringify({event: 'success'}));
       } catch(e) {
         response.writeHead(400, {'Content-Type': 'text/plain'});
         response.end("Couldn't parse request");
       }
+      if (!data.event) {
+        response.writeHead(400, {'Content-Type': 'text/plain'});
+        response.end("Not an event");
+      }
+      console.log('HTTP event: ' + data.event);
+      console.log('WSS clients: ' + wss.clients.size);
+      // Send the event to clients. 
+      wss.clients.forEach(function each(client) {
+        client.send(JSON.stringify({"event": data.event}));
+      });
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end(JSON.stringify({event: 'success'}));
     });
   } else {
+    // Respond to everything else. 
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("I'm listening\n\n");
     response.end("...\n");
@@ -47,34 +65,34 @@ var server = http.createServer(function (request, response) {
 });
 
 server.listen(PORT, function () {
-  console.log('Server is listening on port ' + PORT);
+  console.log('HTTP server is listening on port ' + PORT);
 });
 
-// Handle websockets. 
-console.log('Setting up socket on ' + PORT);
-var wss = new WebSocket.Server({
+// Handle websockets once the http server is listening. 
+console.log('WSS setting up socket on ' + PORT);
+wss = new WebSocket.Server({
   server: server,
   autoAcceptConnections: false,
   clientTracking: true
 });
-console.log('Socket listening on ' + PORT);
+console.log('WSS socket listening on ' + PORT);
 
 wss.on('connection', function connection(ws) {
   // Log incoming messages. TODO: more functionality. 
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  ws.on('message', function(message) {
+    logClient(ws, 'Received message: ' + message);
   });
   // Logging other events. 
-  ws.on('close', function incoming(code, reason) {
+  ws.on('close', function(code, reason) {
     logClient(ws, 'Closed. Code: ' + code + '. Reason: ' + reason);
   });
-  ws.on('error', function incoming(error) {
+  ws.on('error', function(error) {
     logClient(ws, 'Error: ' + error);
   });
-  ws.on('unexpected-response', function incoming(request, response) {
+  ws.on('unexpected-response', function(request, response) {
     logClient(ws, 'unexpected-response');
   });
-  ws.on('upgrade', function incoming(response) {
+  ws.on('upgrade', function(response) {
     logClient(ws, 'upgrade');
   });
   // Keep track of live connections. 
@@ -84,24 +102,27 @@ wss.on('connection', function connection(ws) {
   ws.send(JSON.stringify({"status": "Connected"}));
 });
 
-function logClient(ws, message) {
-  console.log('Client ' + ws._socket.remoteAddress + ':' + ws._socket.remotePort + ' ' + message);
-}
-
 wss.on('error', function(error) {
-  console.log('ERROR: ' + error);
+  console.log('WSS ERROR: ' + error);
 });
 
 wss.on('close', function(close) {
-  console.log('Close: ' + close);
+  console.log('WSS close: ' + close);
 });
 
 // Detect broken connections. 
-function noop() {}
 const interval = setInterval(function ping() {
   wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
+    if (ws.isAlive === false) {
+      logClient(ws, 'died. Terminating connection')
+      return ws.terminate();
+    }
     ws.isAlive = false;
     ws.ping(noop);
   });
 }, 30000);
+
+function noop() {}
+function logClient(ws, message) {
+  console.log('Client ' + ws._socket.remoteAddress + ':' + ws._socket.remotePort + ' ' + message);
+}
